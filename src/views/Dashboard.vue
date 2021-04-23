b8aec2f1473d212c88ea042c58f664a0c51ceaec
Martijn Faber <martijn@fabtronics.nl>
2021-04-23 18:58:55 +0200

    feat: added the dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Goedenmorgen {{ name }}.</h1>
+    <Widget></Widget>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Widget from "../components/Widget.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Widget,
+  },
+  data() {
+    return {
+      name: "Martijn",
+    };
+  },
+});
+</script>
